c9ec309a094ee201ec5d14bdd7b40d2c4ff86916
Han Pengfei <pengphei@foxmail.com>
2019-12-30 23:40:39 +0800

    uboot: update mainline building script
    Signed-off-by: Han Pengfei <pengphei@foxmail.com>

--- build_uboot.sh
+++ build_uboot.sh
@@ -6,8 +6,10 @@ GCC_AARCH64=gcc-arm-9.2-2019.12-x86_64-aarch64-none-elf
 # export toolchains 
 export PATH=$PWD/build/tools/${GCC_AARCH64}/bin:$PATH
 
+export UBOOTDIR=$PWD/aml-uboot-legacy
 export FIPDIR=$PWD/aml-uboot-legacy/fip
 export FIPDIR_ML=$PWD/aml-uboot/fip
+export ROOTDIR=$PWD
 
 # build mainline uboot
 cd $PWD/aml-uboot
@@ -18,15 +20,23 @@ make ARCH=arm CROSS_COMPILE=aarch64-none-elf-
 mkdir -p $FIPDIR_ML
 
 # Copy legacy fip referring to mainline
-cp $FIPDIR/gxl/bl2.bin $FIPDIR_ML/
-cp $FIPDIR/gxl/acs.bin $FIPDIR_ML/
-cp $FIPDIR/gxl/bl21.bin $FIPDIR_ML/
-cp $FIPDIR/gxl/bl30.bin $FIPDIR_ML/
-cp $FIPDIR/gxl/bl301.bin $FIPDIR_ML/
-cp $FIPDIR/gxl/bl31.img $FIPDIR_ML/
+cp $UBOOTDIR/build/scp_task/bl301.bin $FIPDIR_ML/
+cp $UBOOTDIR/build/board/khadas/kvim3/firmware/acs.bin $FIPDIR_ML/
+cp $FIPDIR/g12b/bl2.bin $FIPDIR_ML/
+cp $FIPDIR/g12b/bl30.bin $FIPDIR_ML/
+cp $FIPDIR/g12b/bl31.img $FIPDIR_ML/
+cp $FIPDIR/g12b/ddr3_1d.fw $FIPDIR_ML/
+cp $FIPDIR/g12b/ddr4_1d.fw $FIPDIR_ML/
+cp $FIPDIR/g12b/ddr4_2d.fw $FIPDIR_ML/
+cp $FIPDIR/g12b/diag_lpddr4.fw $FIPDIR_ML/
+cp $FIPDIR/g12b/lpddr3_1d.fw $FIPDIR_ML/
+cp $FIPDIR/g12b/lpddr4_1d.fw $FIPDIR_ML/
+cp $FIPDIR/g12b/lpddr4_2d.fw $FIPDIR_ML/
+cp $FIPDIR/g12b/piei.fw $FIPDIR_ML/
+cp $FIPDIR/g12b/aml_ddr.fw $FIPDIR_ML/
 cp u-boot.bin $FIPDIR_ML/bl33.bin
 
-$FIPDIR/blx_fix.sh \
+$ROOTDIR/blx_fix.sh \
 	$FIPDIR_ML/bl30.bin \
 	$FIPDIR_ML/zero_tmp \
 	$FIPDIR_ML/bl30_zero.bin \
@@ -35,24 +45,44 @@ $FIPDIR/blx_fix.sh \
 	$FIPDIR_ML/bl30_new.bin \
 	bl30
 
-python $FIPDIR/acs_tool.pyc $FIPDIR_ML/bl2.bin $FIPDIR_ML/bl2_acs.bin $FIPDIR_ML/acs.bin 0
-
-$FIPDIR/blx_fix.sh \
-	$FIPDIR_ML/bl2_acs.bin \
+$ROOTDIR/blx_fix.sh \
+	$FIPDIR_ML/bl2.bin \
 	$FIPDIR_ML/zero_tmp \
 	$FIPDIR_ML/bl2_zero.bin \
-	$FIPDIR_ML/bl21.bin \
+	$FIPDIR_ML/acs.bin \
 	$FIPDIR_ML/bl21_zero.bin \
 	$FIPDIR_ML/bl2_new.bin \
 	bl2
 
-$FIPDIR/gxl/aml_encrypt_gxl --bl3enc --input $FIPDIR_ML/bl30_new.bin
-$FIPDIR/gxl/aml_encrypt_gxl --bl3enc --input $FIPDIR_ML/bl31.img
-$FIPDIR/gxl/aml_encrypt_gxl --bl3enc --input $FIPDIR_ML/bl33.bin
-$FIPDIR/gxl/aml_encrypt_gxl --bl2sig --input $FIPDIR_ML/bl2_new.bin --output $FIPDIR_ML/bl2.n.bin.sig
-$FIPDIR/gxl/aml_encrypt_gxl --bootmk \
+$UBOOTDIR/fip/g12b/aml_encrypt_g12b --bl30sig --input $FIPDIR_ML/bl30_new.bin \
+					--output $FIPDIR_ML/bl30_new.bin.g12a.enc \
+					--level v3
+$UBOOTDIR/fip/g12b/aml_encrypt_g12b --bl3sig --input $FIPDIR_ML/bl30_new.bin.g12a.enc \
+					--output $FIPDIR_ML/bl30_new.bin.enc \
+					--level v3 --type bl30
+ $UBOOTDIR/fip/g12b/aml_encrypt_g12b --bl3sig --input $FIPDIR_ML/bl31.img \
+					--output $FIPDIR_ML/bl31.img.enc \
+					--level v3 --type bl31
+$UBOOTDIR/fip/g12b/aml_encrypt_g12b --bl3sig --input $FIPDIR_ML/bl33.bin --compress lz4 \
+					--output $FIPDIR_ML/bl33.bin.enc \
+					--level v3 --type bl33 --compress lz4
+ $UBOOTDIR/fip/g12b/aml_encrypt_g12b --bl2sig --input $FIPDIR_ML/bl2_new.bin \
+					--output $FIPDIR_ML/bl2.n.bin.sig
+
+ $UBOOTDIR/fip/g12b/aml_encrypt_g12b --bootmk \
 		--output $FIPDIR_ML/u-boot.bin \
 		--bl2 $FIPDIR_ML/bl2.n.bin.sig \
 		--bl30 $FIPDIR_ML/bl30_new.bin.enc \
 		--bl31 $FIPDIR_ML/bl31.img.enc \
-		--bl33 $FIPDIR_ML/bl33.bin.enc
+		--bl33 $FIPDIR_ML/bl33.bin.enc \
+		--ddrfw1 $FIPDIR_ML/ddr4_1d.fw \
+		--ddrfw2 $FIPDIR_ML/ddr4_2d.fw \
+		--ddrfw3 $FIPDIR_ML/ddr3_1d.fw \
+		--ddrfw4 $FIPDIR_ML/piei.fw \
+		--ddrfw5 $FIPDIR_ML/lpddr4_1d.fw \
+		--ddrfw6 $FIPDIR_ML/lpddr4_2d.fw \
+		--ddrfw7 $FIPDIR_ML/diag_lpddr4.fw \
+		--ddrfw8 $FIPDIR_ML/aml_ddr.fw \
+		--ddrfw9 $FIPDIR_ML/lpddr3_1d.fw \
+		--level v3
+
